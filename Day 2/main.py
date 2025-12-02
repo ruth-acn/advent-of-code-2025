3c7ac5a167f1bc4510b35c4f5a13686f721a4243
Ruth Faherty <ruth.faherty@accenture.com>
2025-12-02 12:16:05 +0000

    Day 2: puzzle 2

--- Day 2/main.py
+++ Day 2/main.py
@@ -31,6 +31,54 @@ def puzzle_one():
     
     return invalid_count
 
+def puzzle_two():
+    ids = read_ids()
+    ids = [i.split("-") for i in ids]
+
+    invalid_count = 0
+    
+    for i in ids:
+
+        id_low = i[0]
+        id_high = i[1]
+
+        num_digits = {}
+
+        invalid_ids = []
+
+        if len(id_low) != len(id_high):
+            num_digits[len(id_low)] = {'max':'9' * len(id_low), 'min': id_low}
+            num_digits[len(id_high)] = {'max': id_high, 'min': '1' + '0' * (len(id_high)-1)}
+
+        else:
+            num_digits[len(id_low)] = {'max': id_high, 'min': id_low}
+
+        for n in num_digits:
+            id_max = num_digits[n]['max']
+            id_min = num_digits[n]['min']
+            
+            factors = []
+            for i in range(1, n):
+                if n % i == 0:
+                    factors.append(i)
+
+            for f in factors:
+                start_high = id_max[0:f]
+                start_low = id_min[0:f]
+
+                r = n // f
+
+                for j in range(int(start_low),int(start_high)+1):
+                    num = int(str(j) * r)
+                    if (num <= int(id_max)) & (num >= int(id_min)):
+                        invalid_ids.append(num)
+
+        invalid_ids = list(set(invalid_ids))
+        invalid_count += sum(invalid_ids)
+
+
+    return invalid_count
 
 if __name__ == '__main__':
     print(f"Solution 1: {puzzle_one()}")
+    print(f"Solution 2: {puzzle_two()}")
